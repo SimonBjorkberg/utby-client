a84052c4f1edd9e79a3d69dc9dd042db0cbd60c9
SimonBjorkberg <S.bjorkberg@gmail.com>
2024-04-15 16:11:20 +0200

    new stroke width for smaller screens

--- src/app/components/Path.tsx
+++ src/app/components/Path.tsx
@@ -10,7 +10,7 @@ interface Props {
 export default function Path({ boulder, setSelectedPath, selectedPath }: Props) {
     return (
         <>
-            <path onClick={() => setSelectedPath(boulder.id)} className={`${selectedPath === boulder.id ? "stroke-red-500 opacity-100 z-10 stroke-[2.5]" : "opacity-70 stroke-neutral-800 hover:cursor-pointer z-0 stroke-2"}`} fill="none" stroke="red" d={boulder.path} />
+            <path onClick={() => setSelectedPath(boulder.id)} className={`${selectedPath === boulder.id ? "stroke-red-500 opacity-100 z-10 stroke-[4] lg:stroke-[2.5]" : "opacity-70 stroke-neutral-800 hover:cursor-pointer z-0 stroke-2"}`} fill="none" stroke="red" d={boulder.path} />
         </>
     )
 }
