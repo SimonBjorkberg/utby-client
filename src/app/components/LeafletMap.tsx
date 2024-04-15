3d1fa238cd54b5dfdecce9315143d9d76f943797
SimonBjorkberg <S.bjorkberg@gmail.com>
2024-04-15 22:42:42 +0200

    geolocation first iteration

--- src/app/components/LeafletMap.tsx
+++ src/app/components/LeafletMap.tsx
@@ -5,18 +5,34 @@ import { data } from '../../../public/data/data'
 import { MapContainer, TileLayer, Marker, Circle, Popup } from 'react-leaflet'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+interface Location {
+    lat: number,
+    lon: number,
+}
 
 export default function LeafletMap() {
-
     const router = useRouter()
+    const [location, setLocation] = useState<Location | undefined>(undefined)
+
+    useEffect(() => {
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(({ coords }) => {
+                const { latitude, longitude } = coords
+                setLocation({lat: latitude, lon: longitude})
+            })
+        }
+    }, [])
 
     return (
         <MapContainer className='w-full h-full z-0' center={[57.74494299413902, 12.057094987938887]} zoom={16} scrollWheelZoom={true}>
             <TileLayer
                 url="https://maps.infra.entryscape.com/maps/basic/{z}/{x}/{y}.png?key=srAw96F43apXJCHhfWnu"
             />
+            {location && <Circle className='animate-ping' center={[location.lat, location.lon]} radius={2} />}
             {data.map((section, i) => {
-                return <Circle key={i} center={[section.position.lat, section.position.lng]} radius={4}>
+                return <Circle key={i} center={[section.position.lat, section.position.lng]} radius={6}>
                     <Popup>
                         <div className="flex flex-col w-[250px]">
                             <h1 className="font-bold text-center text-[15px] mb-1">{section.title}</h1>
