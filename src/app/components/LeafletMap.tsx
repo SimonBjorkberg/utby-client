7971b68e88fc6541b188a0488fc6f8537d44406b
SimonBjorkberg <S.bjorkberg@gmail.com>
2024-04-15 23:08:57 +0200

    attempting the geolocation again

--- src/app/components/LeafletMap.tsx
+++ src/app/components/LeafletMap.tsx
@@ -7,30 +7,56 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
-interface Location {
-    lat: number,
-    lon: number,
+interface LocationState {
+    latitude: number | null;
+    longitude: number | null;
+    error: string | null;
 }
 
 export default function LeafletMap() {
     const router = useRouter()
-    const [location, setLocation] = useState<Location | undefined>(undefined)
+    const [location, setLocation] = useState<LocationState | undefined>(undefined);
 
     useEffect(() => {
-        if ('geolocation' in navigator) {
-            navigator.geolocation.getCurrentPosition(({ coords }) => {
-                const { latitude, longitude } = coords
-                setLocation({lat: latitude, lon: longitude})
-            })
+        const handleLocationChange = (position: GeolocationPosition) => {
+            setLocation({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                error: null,
+            });
+        };
+
+        const handleLocationError = (error: GeolocationPositionError) => {
+            setLocation({
+                latitude: null,
+                longitude: null,
+                error: error.message,
+            });
+        };
+
+        if (!navigator.geolocation) {
+            setLocation({
+                latitude: null,
+                longitude: null,
+                error: 'Geolocation is not supported by your browser.',
+            });
+        } else {
+            const watcher = navigator.geolocation.watchPosition(
+                handleLocationChange,
+                handleLocationError,
+                { enableHighAccuracy: true, maximumAge: 30000, timeout: 27000 }
+            );
+
+            return () => navigator.geolocation.clearWatch(watcher);
         }
-    }, [])
+    }, []);
 
     return (
         <MapContainer className='w-full h-full z-0' center={[57.74494299413902, 12.057094987938887]} zoom={16} scrollWheelZoom={true}>
             <TileLayer
                 url="https://maps.infra.entryscape.com/maps/basic/{z}/{x}/{y}.png?key=srAw96F43apXJCHhfWnu"
             />
-            {location && <Circle className='animate-ping' center={[location.lat, location.lon]} radius={2} />}
+            {location && <Circle center={[location.latitude ?? 0, location.longitude ?? 0]} radius={3} />}
             {data.map((section, i) => {
                 return <Circle key={i} center={[section.position.lat, section.position.lng]} radius={6}>
                     <Popup>
