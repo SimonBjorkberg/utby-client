9a42a8fc4557b34c6cf39a92303a8c923eb08409
SimonBjorkberg <S.bjorkberg@gmail.com>
2024-04-17 06:41:53 +0200

    body background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
+      <body className={`bg-orange-500 ${inter.className}`}>{children}
       </body>
     </html>
   );
